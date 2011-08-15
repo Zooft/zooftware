d9165014325f9e911e3214ca299f1ad1305c7e78
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-08-15 19:49:54 +0200

    dummy user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Factory.create(:user)
+  end
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
 end
