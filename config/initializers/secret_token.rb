e6933f6f797a844e99e77dc8b3bba98835e425d7
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-08-15 15:44:57 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zooftware::Application.config.secret_token = '7ace3a9db47bc1c2c106369b28d54f313468bc9812182501cb554b0ff259e1c8b42182165369b840da9070e2b01a04b5fe637d2bfa674c672d72e867c1e77f2a'
